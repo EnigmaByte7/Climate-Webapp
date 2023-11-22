3bdbbd85e80c4b4b4b89c3b441d5bd60ff95a465
EnigmaByte7 <134928189+EnigmaByte7@users.noreply.github.com>
2023-11-23 01:12:20 +0530

    remote transfer

---
+++ param.js
@@ -0,0 +1,89 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const params = new URLSearchParams(window.location.search);
+
+    const city = params.get('city');
+    const temperature = params.get('temperature');
+    const description = params.get('description').toUpperCase();
+    let aqi=params.get('aqi');
+    const wind=params.get('speed');
+    let snow=params.get('snow');
+    let rain=params.get('rain');
+    const icon=params.get('ikey');
+    const humidity=params.get('humidity');
+    let main=params.get('main');
+    let color1, color2;
+    if (['Haze','Mist','Fog'].includes(main)){
+        main='Haze';
+        color1='Silver';
+        color2='WhiteSmoke';
+    }
+    else if(['Smoke','Dust','Sand'].includes(main)){
+        main='Smoke';
+        color1='Silver';
+        color2='WhiteSmoke';
+    }
+    else if (['Torando','Squall','Ash'].includes(main)){
+        main='Tornado';
+        color1='Navy';
+        color2='MediumTurquoise';
+    }
+    else if(main=='Rain')
+    {
+        color1='Navy';
+        color2='MediumTurquoise';
+    }
+    else if(main=='Sunny')
+    {
+        color1='DarkOrange';
+        color2='LightYellow';
+    }
+    else if(main=='Snow')
+    {
+        color1='LightBlue';
+        color2='Snow';
+    }
+    else if(main=='Clear')
+    {
+        color1='DodgerBlue';
+        color2='White';
+    }
+    else if (main=='Clouds')
+    {
+        color1='LightSlateGray';
+        color2='LightGray';
+    }
+    else if(main=='Thunderstorm'){
+        color1='DimGray';
+        color2='SeaShell';
+    }
+    else if(main=='Drizzle')
+    {
+        color1='MediumTurquoise';
+        color2='MintCream';
+    }
+
+    if (rain["1h"]=="undefined")
+    {
+        rain=0;
+    }
+    if (snow=="undefined")
+    {
+        snow=0;
+    }
+    let index=['0','Good','Fair','Moderate','Poor','Very Poor'];
+    let color=['0','Green','Yellow','Orange','Red','Maroon'];
+
+    document.getElementById('city').innerText = city;
+    document.getElementById('temp').innerText = Math.round(temperature)+" ºC";
+    document.getElementById('description').innerText = description;
+    document.getElementById('aqitext').innerText=index[aqi];
+    document.getElementById('aqitext').style.color=color[aqi];
+    document.getElementById('wtext').innerText=wind+" Km/h";
+    document.getElementById('hum').innerText=humidity+" %";
+    document.getElementById('rain').innerText=rain;
+    document.getElementById('stext').innerText=snow;
+    document.getElementById('icon').src=`https://openweathermap.org/img/wn/${icon}@2x.png`;
+    document.getElementById('status').src=`/Climate/static/${main}.png`;
+    document.body.style.backgroundImage = `linear-gradient(to bottom, ${color1}, ${color2})`;
+});
---
+++ request.js
@@ -0,0 +1,42 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('form');
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const city = document.getElementById('city').value;
+            const apiKey = 'd2f83ba938e9f6cb787ac12e31709d38';
+            const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => {
+
+                    const weatherInfo = {
+                        city: data.name,
+                        temperature: data.main.temp,
+                        humidity:data.main.humidity,
+                        description: data.weather[0].description,
+                        wind:data.wind.speed,
+                        rain:data.rain,
+                        snow:data.snow,
+                        icon:data.weather[0].icon,
+                        main:data.weather[0].main,
+                        lat:data.coord.lat,
+                        lon:data.coord.lon
+                        console.log('Response:',data)
+                    };
+            console.log('Weather Info:', weatherInfo);
+            const aqiUrl = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${encodeURIComponent(weatherInfo.lat)}&lon=${encodeURIComponent(weatherInfo.lon)}&appid=${apiKey}`;
+            fetch(aqiUrl)
+                    .then(response => response.json())
+                    .then(data =>{
+                        const aqidata = {
+                            aqi:data.list[0].main.aqi
+                        };
+                    window.location.href = `weather.html?city=${encodeURIComponent(weatherInfo.city)}&temperature=${encodeURIComponent(weatherInfo.temperature)}&description=${encodeURIComponent(weatherInfo.description)}&aqi=${encodeURIComponent(aqidata.aqi)}&speed=${encodeURIComponent(weatherInfo.wind)}&rain=${encodeURIComponent(weatherInfo.rain)}&snow=${encodeURIComponent(weatherInfo.snow)}&ikey=${encodeURIComponent(weatherInfo.icon)}&humidity=${encodeURIComponent(weatherInfo.humidity)}&main=${encodeURIComponent(weatherInfo.main)}`;
+                })
+                .catch(error => {
+                    console.error('Error fetching weather data:', error);
+                });
+        })
+    });
+})
